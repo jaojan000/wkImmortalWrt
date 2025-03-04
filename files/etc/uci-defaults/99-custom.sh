d7dead7a4c1f2866ffa2d3d328581d99a90342b6
Camelo Davis <52444324+jaojan000@users.noreply.github.com>
2025-03-04 17:52:50 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -70,10 +70,10 @@ elif [ "$count" -gt 1 ]; then
    # LAN口设置静态IP
    uci set network.lan.proto='static'
    # 多网口设备 支持修改为别的ip地址
-   uci set network.lan.ipaddr='192.168.8.1'
+   uci set network.lan.ipaddr='192.168.8.5'
    uci set network.lan.netmask='255.255.255.0'
    uci set network.lan.gateway='192.168.8.18'
-   echo "set 192.168.8.1 at $(date)" >> $LOGFILE
+   echo "set 192.168.8.5 at $(date)" >> $LOGFILE
    # 判断是否启用 PPPoE
    echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    if [ "$enable_pppoe" = "yes" ]; then
